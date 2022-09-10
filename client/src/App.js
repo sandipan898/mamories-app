7197751d1bcef440106e997f70a165b918557096
sandipan898 <sandipan.das898@gmail.com>
2022-09-11 00:41:07 +0530

    setting up styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" heigh="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          heigh="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
@@ -24,7 +31,7 @@ const App = () => {
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} sm={4}>
               <Form />
             </Grid>
           </Grid>
